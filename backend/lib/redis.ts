d53f1f3165decb70d266ef3deaf1ed7cb1e08c82
Adeniji Olajide <adenijiolajid01@gmail.com>
2026-06-30 16:01:15 +0100

    feat: add Redis password configuration and enhance installation scripts for client and server

--- backend/lib/redis.ts
+++ backend/lib/redis.ts
@@ -1,5 +1,6 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({ port: Number(process.env.REDIS_PORT), host: process.env.REDIS_HOST, maxRetriesPerRequest: null });
+const redis = new Redis({ port: Number(process.env.REDIS_PORT), host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD, maxRetriesPerRequest: null });
 
 export default redis;
